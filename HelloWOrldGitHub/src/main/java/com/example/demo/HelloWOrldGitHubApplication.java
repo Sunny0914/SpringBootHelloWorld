f1d262c1bb2d0ee63f2eb618596dbd5972febe81
Sunny0914 <sanidesale2002@gmail.com>
2025-04-22 13:48:53 +0530

    First Commit

---
+++ HelloWOrldGitHub/src/main/java/com/example/demo/HelloWOrldGitHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWOrldGitHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWOrldGitHubApplication.class, args);
+	}
+
+}
